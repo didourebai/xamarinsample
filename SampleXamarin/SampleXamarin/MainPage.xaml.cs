2cdb57b0d3ba19999e4d31f1c26092bfa2439c3e
Hamida REBAI <didourebai@gmail.com>
2018-12-12 22:28:33 -0500

    (+) add new repository

---
+++ SampleXamarin/SampleXamarin/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public List<string> Governorate { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Governorate = new List<string>
+            {
+                "أريانة",
+                "باجة",
+                "بنزرت",
+                "قابس",
+                "قفصة",
+                "جندوبة",
+                "القيروان",
+                "القصرين",
+                "قبلي",
+                "الكاف",
+                "المهدية",
+                "منوبة",
+                "مدنين",
+                "المنستير",
+                "نابل",
+                "صفاقس",
+                "سيدي بوزيد",
+                "سليانة",
+                "سوسة",
+                "تطاوين",
+                "توزر",
+                "تونس",
+                "زغوان"
+            };
+
+            governorate.BindingContext = this;
+            governorate.ItemsLayout = new ListItemsLayout(ItemsLayoutOrientation.Vertical); // Vertical is default
+        }
+    }
+}
